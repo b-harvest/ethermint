79ac55183426b8f45c1006349cb6f49448ed966c
dudong2 <leesj9476lsj@gmail.com>
2024-01-15 18:58:18 +0900

    test: revert changed num

--- x/feemarket/keeper/abci_test.go
+++ x/feemarket/keeper/abci_test.go
@@ -23,7 +23,7 @@ func (suite *KeeperTestSuite) TestEndBlock() {
 			"pass",
 			false,
 			func() {
-				meter := storetypes.NewGasMeter(uint64(875000001))
+				meter := storetypes.NewGasMeter(uint64(1000000000))
 				suite.ctx = suite.ctx.WithBlockGasMeter(meter)
 				suite.app.FeeMarketKeeper.SetTransientBlockGasWanted(suite.ctx, 5000000)
 			},
